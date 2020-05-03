3d039252ad01dc01e8eed697b918d992629e0bce
pixie <eugenmac56@gmail.ru>
2020-05-03 12:42:26 +0300

    * code refactoring

--- src/components/button-app-bar/subcomponents/Avatar.jsx
+++ src/components/button-app-bar/subcomponents/Avatar.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TooltipTrigger from 'react-popper-tooltip';
 import {Trigger} from './Trigger';
-import {CustomTooltip} from './Tooltip';
+import {CustomTooltip} from './CustomTooltip';
 
 export const Avatar = ({setAuthorizedHandler}) => {
+    const [showTooltip, setShowTooltip] = useState(false);
     return (
         <TooltipTrigger
-            placement="bottom"
-            trigger="click"
+            placement='bottom'
+            trigger='click'
             tooltip={CustomTooltip(setAuthorizedHandler)}
         >
             {Trigger}
